feb829044e6f3c1929e04035545ec01121d479c4
Apprentice <apprentice@dbc-chicago-009.local>
2013-08-19 15:55:54 -0500

    tweets retrieving, no caching

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,9 +1,2 @@
 
-def client
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key = "fWVLFOjEyCu9HUbmrbQQ"
-    config.consumer_secret = "2uFXgpQIszGeruUMZ1mpz8iN01YlkgjWI8AcZ9MOhc"
-    config.access_token = "54656020-ykWvUUWcr2e5HJ9jHwVwmMPpGMmiJOCOBYVFLlRq4"
-    config.access_token_secret = "SXtuNiflJ2tqXVBKA0yvU8Y1q2xB4YqqVfElJMMquw"
-  end  
-end
+   
--- config/environment.rb
+++ config/environment.rb
@@ -33,3 +33,9 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
+CLIENT = Twitter::REST::Client.new do |config|
+    config.consumer_key = "fWVLFOjEyCu9HUbmrbQQ"
+    config.consumer_secret = "2uFXgpQIszGeruUMZ1mpz8iN01YlkgjWI8AcZ9MOhc"
+    config.access_token = "54656020-ykWvUUWcr2e5HJ9jHwVwmMPpGMmiJOCOBYVFLlRq4"
+    config.access_token_secret = "SXtuNiflJ2tqXVBKA0yvU8Y1q2xB4YqqVfElJMMquw"
+  end 
